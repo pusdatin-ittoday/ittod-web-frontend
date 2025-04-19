1e21e0f085fa33425c2c121286641705921ed468
Daulyoi <derp.daffnet@gmail.com>
2025-04-19 07:39:08 +0700

    made competition page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Event from './pages/Event'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Lomba from './pages/Lomba';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
             <Route path="/" element={<Home />} /> 
             <Route path="/home" element={<Home />} /> 
             <Route path="/event" element={<Event />} /> 
+            <Route path="/competition" element={<Lomba />} /> 
           </Routes>
       </BrowserRouter>
     </>
--- src/pages/Lomba.jsx
+++ src/pages/Lomba.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import EventCard from '../components/EventCard'
 
 const Lomba = () => {
   return (
-    <div>Lomba</div>
+    <div className='min-h-screen min-w-screen flex flex-col items-center justify-center py-16 px-4 gap-8'>
+      <h1 className='text-white text-5xl font-playfair font-bold leading-[140.625%] [text-shadow:0px_5px_10px_rgba(172,104,113,0.7)]'>
+        Competition
+      </h1>
+      
+      {/* competitions */}
+      <div className='flex flex-wrap justify-center align-middle gap-10'>
+        <EventCard
+          title= "Hack Today"
+          description= "Acara seru banget aduhai ayo join ittod yang tahun ini"
+          imageSrc={"/DummyImg.jpg"}
+        />
+        <EventCard
+          title= "Mine Today"
+          description= "Acara seru banget aduhai ayo join ittod yang tahun ini"
+          imageSrc={"/DummyImg.jpg"}
+        />
+        <EventCard
+          title= "UX Today"
+          description= "Acara seru banget aduhai ayo join ittod yang tahun ini"
+          imageSrc={"/DummyImg.jpg"}
+        />
+        <EventCard
+          title= "Game Today"
+          description= "Acara seru banget aduhai ayo join ittod yang tahun ini"
+          imageSrc={"/DummyImg.jpg"}
+        />
+      </div>
+    </div>
   )
 }
 
