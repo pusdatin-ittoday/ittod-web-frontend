38cd8d2b683a536841c3adf07d766d970362f5d3
julius_Calvin <juliuscalvin_kurniadi@apps.ipb.ac.id>
2025-04-20 12:16:52 +0700

    Adding custom-button-bg in index.css, adding route in App.jsx, installing React Icons, adding google.svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Event from './pages/Event'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/LoginPage/Login';
+import Register from './pages/LoginPage/Register';
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
             <Route path="/" element={<Home />} /> 
             <Route path="/home" element={<Home />} /> 
             <Route path="/event" element={<Event />} /> 
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
       </BrowserRouter>
     </>
