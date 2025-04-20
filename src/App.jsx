631a665de06ed8352ffd330518a468df0cc49df7
TaufiqSadri <sadritaufiq@gmail.com>
2025-04-20 12:20:09 +0700

    Menambahkan fitur Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import Event from './pages/Event'
+import Event from './pages/Event';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Lomba from './pages/Lomba';
 import TimelineUmum from './pages/TimelineUmum';
+import ContactUs from './pages/ContactUs';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} /> 
-            <Route path="/home" element={<Home />} /> 
-            <Route path="/event" element={<Event />} /> 
-            <Route path="/competition" element={<Lomba />} /> 
-            <Route path="/timeline" element={<TimelineUmum />}  /> 
-          </Routes>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/home"
+            element={<Home />}
+          />
+          <Route
+            path="/event"
+            element={<Event />}
+          />
+          <Route
+            path="/competition"
+            element={<Lomba />}
+          />
+          <Route
+            path="/timeline"
+            element={<TimelineUmum />}
+          />
+          <Route
+            path="/ContactUs"
+            element={<ContactUs />}
+          />
+        </Routes>
       </BrowserRouter>
     </>
   );
 };
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="h-[2000px] bg-gradient-to-br from-[#6a316c] to-[#2e1c3d]"></div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
