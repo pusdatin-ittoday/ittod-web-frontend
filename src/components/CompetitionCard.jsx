85536e0b09c202d52c0b0471f2578788c2419533
Daulyoi <derp.daffnet@gmail.com>
2025-04-28 21:52:28 +0700

    benerin responsive compe (bukan satuan) sama event

--- src/components/CompetitionCard.jsx
+++ src/components/CompetitionCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const EventCard = ({ title, description, imageSrc }) => {
+const CompetitionCard = ({ title, description, imageSrc }) => {
   return (
     <div className="flex lg:gap-5 gap-2 justify-center items-center flex-col w-72 h-[400px] lg:h-[500px] bg-[#4D3159] backdrop-blur-md px-3 lg:px-6 py-6 lg:py-8 rounded-2xl shadow-xl text-center text-white [box-shadow:0_0_15px_5px_#AC6871,_0_0_75px_5px_#AC6871_inset] ">
       <h2 className="text-white lg:text-3xl text-2xl text-center font-semibold font-playfair leading-12 [text-shadow:0_4px_10px_#ac6871]">{title}</h2>
@@ -11,10 +11,10 @@ const EventCard = ({ title, description, imageSrc }) => {
       
       {/* perlu diganti jadi component button */}
       <button className="font-dm-sans font-bold text-sm lg:text-lg bg-gradient-to-r from-[#F97283] via-[#B247B4] to-[#9323C2] text-white py-3 px-4 rounded-xl custom-button-shadow hover:scale-105 transition duration-300 ease-in-out">
-        Daftar Sekarang
+        Lihat Lebih Lanjut
       </button>
     </div>
   );
 };
 
-export default EventCard;
+export default CompetitionCard;
--- src/pages/Lomba.jsx
+++ src/pages/Lomba.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
-import EventCard from '../components/EventCard'
+import CompetitionCard from '../components/CompetitionCard'
 
 const Lomba = () => {
   return (
-    <div className='min-h-screen min-w-screen flex flex-col items-center justify-center py-16 px-4 gap-8'>
-      <h1 className='text-white text-5xl font-playfair font-bold leading-[140.625%] [text-shadow:0px_5px_10px_rgba(172,104,113,0.7)]'>
+    <div className='min-h-screen min-w-screen flex flex-col items-center justify-center py-16 px-4 lg:px-8 gap-8'>
+      <h1 className='text-white text-4xl lg:text-5xl font-playfair font-bold leading-[140.625%] [text-shadow:0px_5px_10px_rgba(172,104,113,0.7)]'>
         Competition
       </h1>
       
       {/* competitions */}
       <div className='flex flex-wrap justify-center align-middle gap-10'>
-        <EventCard
+        <CompetitionCard
           title= "Hack Today"
           description= "Acara seru banget aduhai ayo join ittod yang tahun ini"
           imageSrc={"/DummyImg.jpg"}
         />
-        <EventCard
+        <CompetitionCard
           title= "Mine Today"
           description= "Acara seru banget aduhai ayo join ittod yang tahun ini"
           imageSrc={"/DummyImg.jpg"}
         />
-        <EventCard
+        <CompetitionCard
           title= "UX Today"
           description= "Acara seru banget aduhai ayo join ittod yang tahun ini"
           imageSrc={"/DummyImg.jpg"}
         />
-        <EventCard
+        <CompetitionCard
           title= "Game Today"
           description= "Acara seru banget aduhai ayo join ittod yang tahun ini"
           imageSrc={"/DummyImg.jpg"}
