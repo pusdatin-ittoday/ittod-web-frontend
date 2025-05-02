59972903bfaf67d5bb57ad042f6060eee318bca6
Daulyoi <derp.daffnet@gmail.com>
2025-05-02 20:28:28 +0700

    ubah logo di home

--- src/pages/LandingPage/Home.jsx
+++ src/pages/LandingPage/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     <>
       <Navbar />
       <div className="min-h-screen flex flex-col justify-start items-center pt-20 overflow-auto">
-        <HeroCard Logo={'/DummyImg.jpg'} />
+        <HeroCard Logo={'/LOGO_ITTODAY_2025.png'} />
 
         <div className="w-full px-6 sm:px-8 md:px-10 lg:px-12 xl:px-16 font-dm-sans">
           <div className="max-w-7xl mx-auto bg-[#4D3159] rounded-3xl text-white shadow-[0_0_60px_#AC6871] py-12 px-6 mt-12 mb-20">
