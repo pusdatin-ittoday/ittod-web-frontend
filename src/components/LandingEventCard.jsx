3672f4ac3eda7f2ff527ce845d3a6fb0433077aa
Daulyoi <derp.daffnet@gmail.com>
2025-05-04 09:50:57 +0700

    tweak event card on home

--- src/components/LandingEventCard.jsx
+++ src/components/LandingEventCard.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom';
 
 const LandingEventCard = ({ title, description, imageSrc, linkHref }) => {
   return (
-    <div className="flex lg:gap-5 gap-2 justify-center items-center flex-col w-72 h-[450px] lg:h-[550px] bg-[#4D3159] backdrop-blur-md px-3 lg:px-6 py-6 lg:py-8 rounded-2xl shadow-xl text-center text-white [box-shadow:0_0_15px_5px_#AC6871,_0_0_75px_5px_#AC6871_inset] ">
+    <div className="flex lg:gap-5 gap-2 justify-center items-center flex-col w-72 h-[400px] lg:h-[550px] bg-[#4D3159] backdrop-blur-md px-3 lg:px-6 py-6 lg:py-8 rounded-2xl shadow-xl text-center text-white [box-shadow:0_0_15px_5px_#AC6871,_0_0_75px_5px_#AC6871_inset] ">
       <h2 className="text-white lg:text-3xl text-2xl text-center font-semibold font-playfair leading-12 [text-shadow:0_4px_10px_#ac6871] truncate">{title}</h2>
 
-      <div className="w-60 h-60 rounded-full overflow-hidden">
+      <div className="lg:w-60 w-40 lg:h-60 h-40 rounded-full overflow-hidden">
         <img
           src={imageSrc}
-          className="w-full h-full object-contain"
+          className="object-cover"
         />
       </div>
 
-      <p className="text-lg lg:text-xl mb-6 font-playfair text-white truncate max-w-full [text-shadow:0_4px_10px_#ac6871]">{description}</p>
+      <div className='h-[20%] flex items-center justify-center'>
+        <p className="text-sm lg:text-lg mb-6 font-playfair text-white max-w-full text-center [text-shadow:0_4px_10px_#ac6871]">{description}</p>
+      </div>
 
       {/* perlu diganti jadi component button */}
       <Link
