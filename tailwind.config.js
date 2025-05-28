3d8569443e47dddcb203495be966c7da711795c8
gamer <ilhamgoesasi@gmail.com>
2025-05-28 14:00:11 +0700

    web was waiting until font was downloaded before loading the page, this makes it load instantly and then swapping to the other font once it finishes downloading

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
     theme: {
       extend: {
         fontFamily: {
-          playfair: ['"Playfair Display"', 'serif'], // <- Tambahkan ini
+          playfair: ['"Playfair Display"', 'serif', { fontDisplay: 'swap' }],
         },
         boxShadow: {
           'glow-white': '0px 1px 6.5px 0px #FF9BA8 inset, 0px 5px 10px 0px rgba(172, 104, 113, 0.70)',
