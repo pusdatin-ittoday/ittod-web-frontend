693bb87167e3465c245668084ba009f2e5d96f87
Aldi Pramudya <llenndesu@gmail.com>
2025-06-13 17:38:48 +0700

    fix: syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
     content: [
       "./index.html",
@@ -15,6 +17,6 @@ export default {
       },
     },
     plugins: [
-      require('tailwind-scrollbar'),
+      tailwindScrollbar,
     ],
   };
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 5173,
   },
   plugins: [
-    react(), 
+    react(),
     tailwindcss()
   ],
 })
