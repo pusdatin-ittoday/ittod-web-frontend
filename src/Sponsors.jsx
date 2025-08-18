f53ec4f0cbd54d958c21dfc9380457103c41089c
Daulyoi <derp.daffnet@gmail.com>
2025-08-19 00:07:03 +0700

    feat: sponsors are now visible on the landing page

--- src/Sponsors.jsx
+++ src/Sponsors.jsx
@@ -1,69 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const Sponsors = () => {
+	const sponsors = [
+		{
+			name: "Sentral Komputer",
+			logo: "/sponsors/Logo-Sentral-Komputer.png",
+		},
+		{
+			name: "Bangungindo",
+			logo: "/sponsors/Logo-Bangunindo.png",
+		},
+		{
+			name: "Intelligo",
+			logo: "/sponsors/Logo-Intelligo.png",
+		},
+	];
 
-    const sponsors = [
-        {
-            name: "Sponsor 1",
-            logo: "/DummyImg.jpg",
-        },
-        {
-            name: "Sponsor 2",
-            logo: "/DummyImg.jpg",
-        },
-        {
-            name: "Sponsor 3",
-            logo: "/DummyImg.jpg",
-        },
-        {
-            name: "Sponsor 4",
-            logo: "/DummyImg.jpg",
-        },
-        {
-            name: "Sponsor 5",
-            logo: "/DummyImg.jpg",
-        },
-        {
-            name: "Sponsor 6",
-            logo: "/DummyImg.jpg",
-        },
-        {
-            name: "Sponsor 5",
-            logo: "/DummyImg.jpg",
-        },
-        {
-            name: "Sponsor 6",
-            logo: "/DummyImg.jpg",
-        },
-        {
-            name: "Sponsor 5",
-            logo: "/DummyImg.jpg",
-        },
-        {
-            name: "Sponsor 6",
-            logo: "/DummyImg.jpg",
-        },
-    ]
+	return (
+		<div className="min-h-screen min-w-screen flex flex-col items-center justify-center py-16 px-4 lg:px-8 gap-8">
+			<h1 className="text-white text-4xl lg:text-5xl font-playfair font-bold leading-[140.625%] [text-shadow:0px_5px_10px_rgba(172,104,113,0.7)]">
+				Our Sponsors
+			</h1>
+			<div>
+				<div className="flex flex-wrap justify-center gap-10 w-full">
+					{sponsors.map((sponsor, index) => (
+						<div
+							key={index}
+							className="flex flex-col items-center justify-center gap-2"
+						>
+							<div className="rounded-full overflow-hidden">
+								<img
+									src={sponsor.logo}
+									className="w-32 h-32 lg:w-48 lg:h-48 object-contain"
+								/>
+							</div>
+							<p className="text-white text-lg font-dm-sans font-semibold">
+								{sponsor.name}
+							</p>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
 
-    return (
-    <div className="min-h-screen min-w-screen flex flex-col items-center justify-center py-16 px-4 lg:px-8 gap-8">
-      <h1 className="text-white text-4xl lg:text-5xl font-playfair font-bold leading-[140.625%] [text-shadow:0px_5px_10px_rgba(172,104,113,0.7)]">
-        Our Sponsors
-      </h1>
-      <div>
-        <div className="flex flex-wrap justify-center gap-10 max-w-7xl w-76 md:w-96 lg:w-192">
-            {sponsors.map((sponsor, index) => (
-                <div key={index} className="flex flex-col items-center justify-center gap-2">
-                <div className="rounded-full overflow-hidden">
-                    <img src={sponsor.logo} className="w-16 lg:w-32 h-16 lg:h-32 object-contain" />
-                </div>
-                <p className="text-white text-lg font-dm-sans font-semibold">{sponsor.name}</p>
-                </div>
-            ))}
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default Sponsors
+export default Sponsors;
--- src/pages/LandingPage/Home.jsx
+++ src/pages/LandingPage/Home.jsx
@@ -1,51 +1,61 @@
-import React from 'react';
-import Navbar from '../../components/Navbar';
-import HeroCard from '../../components/HeroCard';
-import Footer from '../../components/Footer';
-import Event from './Event';
-import Lomba from './Lomba';
-import ContactUs from '../ContactUs';
-import TimelineUmum from '../TimelineUmum';
+import React from "react";
+import Navbar from "../../components/Navbar";
+import HeroCard from "../../components/HeroCard";
+import Footer from "../../components/Footer";
+import Event from "./Event";
+import Lomba from "./Lomba";
+import ContactUs from "../ContactUs";
+import TimelineUmum from "../TimelineUmum";
+import Sponsors from "../../Sponsors";
 
 const Home = () => {
-  return (
-    <>
-      <Navbar />
-      <div
-        className="min-h-screen flex flex-col justify-start items-center pt-20"
-        id="hero"
-      >
-        <HeroCard Logo={'/LOGO_ITTODAY_2025.webp'} />
+	return (
+		<>
+			<Navbar />
+			<div
+				className="min-h-screen flex flex-col justify-start items-center pt-20"
+				id="hero"
+			>
+				<HeroCard Logo={"/LOGO_ITTODAY_2025.webp"} />
 
-
-        <div
-          className="w-full px-6 sm:px-8 md:px-10 lg:px-12 xl:px-16 font-dm-sans"
-          id="about"
-        >
-          <div className="max-w-7xl mx-auto bg-[#4D3159] rounded-3xl text-white shadow-[0_0_60px_#AC6871] py-12 px-6 mt-12 mb-20">
-            <p className="font-dm-sans text-base md:text-lg leading-relaxed text-justify text-pink-100 drop-shadow-[0_0_10px_#ffffff77]">
-              <strong>IT Today</strong> merupakan megaproker yang diselenggarakan oleh Himpunan Mahasiswa Ilmu Komputer (HIMALKOM) IPB dan Departemen Ilmu Komputer IPB yang sudah berlangsung sebanyak 20 kali sejak diselenggarakan pertama
-              kali pada tahun 2003. Acara tahunan ini memiliki serangkaian acara berupa seminar komunitas, workshop, kompetisi, seminar nasional, dan awarding yang dikemas secara menarik. Dengan beragam kegiatan yang melibatkan banyak
-              mahasiswa dan masyarakat umum, acara ini menjadi wadah penting untuk pengembangan pengetahuan dan keterampilan di bidang ilmu komputer serta teknologi.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div id="event">
-        <Event />
-      </div>
-      <div id="competition">
-        <Lomba />
-      </div>
-      <div id="timeline">
-        <TimelineUmum />
-      </div>
-      <div id="contact">
-        <ContactUs />
-      </div>
-      <Footer />
-    </>
-  );
+				<div
+					className="w-full px-6 sm:px-8 md:px-10 lg:px-12 xl:px-16 font-dm-sans"
+					id="about"
+				>
+					<div className="max-w-7xl mx-auto bg-[#4D3159] rounded-3xl text-white shadow-[0_0_60px_#AC6871] py-12 px-6 mt-12 mb-20">
+						<p className="font-dm-sans text-base md:text-lg leading-relaxed text-justify text-pink-100 drop-shadow-[0_0_10px_#ffffff77]">
+							<strong>IT Today</strong> merupakan megaproker yang
+							diselenggarakan oleh Himpunan Mahasiswa Ilmu Komputer (HIMALKOM)
+							IPB dan Departemen Ilmu Komputer IPB yang sudah berlangsung
+							sebanyak 20 kali sejak diselenggarakan pertama kali pada tahun
+							2003. Acara tahunan ini memiliki serangkaian acara berupa seminar
+							komunitas, workshop, kompetisi, seminar nasional, dan awarding
+							yang dikemas secara menarik. Dengan beragam kegiatan yang
+							melibatkan banyak mahasiswa dan masyarakat umum, acara ini menjadi
+							wadah penting untuk pengembangan pengetahuan dan keterampilan di
+							bidang ilmu komputer serta teknologi.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div id="event">
+				<Event />
+			</div>
+			<div id="competition">
+				<Lomba />
+			</div>
+			<div id="timeline">
+				<TimelineUmum />
+			</div>
+			<div id="sponsors">
+				<Sponsors />
+			</div>
+			<div id="contact">
+				<ContactUs />
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default Home;
