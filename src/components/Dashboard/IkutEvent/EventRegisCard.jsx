238010f1435d044d3cad93a1e16d96c94b8878f5
Aldi Pramudya <llenndesu@gmail.com>
2025-09-12 14:28:05 +0700

    chore: changed to 1800

--- src/components/Dashboard/IkutEvent/EventRegisCard.jsx
+++ src/components/Dashboard/IkutEvent/EventRegisCard.jsx
@@ -7,7 +7,7 @@ const events = [
     title: "NATIONAL SEMINAR",
     description: "Wawasan menarik di bidang Teknologi Informasi!",
     image: "/logo-event/SEMINAR-NASIONAL.webp",
-    closeRegistrationDate: new Date("2025-09-12T14:00:00"),
+    closeRegistrationDate: new Date("2025-09-12T18:00:00"),
   },
   {
     title: "BOOTCAMP",
