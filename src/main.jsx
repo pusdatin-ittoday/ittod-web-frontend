a6ab85a814af9bba523fcdc718c2b24bd7bd01f7
DeatWalls-Azka <deatwalls@gmail.com>
2026-06-30 20:50:51 +0700

    new

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,9 @@ import './index.css';
 import App from './App.jsx';
 
 import 'remixicon/fonts/remixicon.css';
-import { NotificationProvider } from './context/NotificationContext.jsx';
-import NotificationToast from './components/Notification/NotificationToast.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NotificationProvider>
-      <App />
-      <NotificationToast />
-    </NotificationProvider>
+    <App />
   </StrictMode>
 );
