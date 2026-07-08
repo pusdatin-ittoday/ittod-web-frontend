df975b755941432e28349d159af5fc7bc6121952
DeatWalls-Azka <deatwalls@gmail.com>
2026-07-08 17:10:15 +0700

    perbaikan keyboard mobile

--- src/components/Dashboard/EditProfil.jsx
+++ src/components/Dashboard/EditProfil.jsx
@@ -478,8 +478,10 @@ class EditProfile extends Component {
                                             <path d="M2 16H3.425L13.2 6.225L11.775 4.8L2 14.575V16ZM0 18V13.75L13.2 0.575C13.4 0.391667 13.6208 0.25 13.8625 0.15C14.1042 0.05 14.3583 0 14.625 0C14.8917 0 15.15 0.05 15.4 0.15C15.65 0.25 15.8667 0.4 16.05 0.6L17.425 2C17.625 2.18333 17.7708 2.4 17.8625 2.65C17.9542 2.9 18 3.15 18 3.4C18 3.66667 17.9542 3.92083 17.8625 4.1625C17.7708 4.40417 17.625 4.625 17.425 4.825L4.25 18H0ZM16 3.4L14.6 2L16 3.4ZM12.475 5.525L11.775 4.8L13.2 6.225L12.475 5.525Z" fill="#464652"/>
                                         </svg>
                                         <input
-                                            type="tel"
-                                            inputMode="tel"
+                                            type="text"
+                                            inputMode="text"
+                                            autoComplete="name"
+                                            autoCapitalize="words"
                                             name="full_name"
                                             placeholder="Nama Lengkap"
                                             value={full_name}
@@ -526,7 +528,9 @@ class EditProfile extends Component {
                                             <path d="M16.95 18C14.8667 18 12.8083 17.5458 10.775 16.6375C8.74167 15.7292 6.89167 14.4417 5.225 12.775C3.55833 11.1083 2.27083 9.25833 1.3625 7.225C0.454167 5.19167 0 3.13333 0 1.05C0 0.75 0.1 0.5 0.3 0.3C0.5 0.1 0.75 0 1.05 0H5.1C5.33333 0 5.54167 0.0791667 5.725 0.2375C5.90833 0.395833 6.01667 0.583333 6.05 0.8L6.7 4.3C6.73333 4.56667 6.725 4.79167 6.675 4.975C6.625 5.15833 6.53333 5.31667 6.4 5.45L3.975 7.9C4.30833 8.51667 4.70417 9.1125 5.1625 9.6875C5.62083 10.2625 6.125 10.8167 6.675 11.35C7.19167 11.8667 7.73333 12.3458 8.3 12.7875C8.86667 13.2292 9.46667 13.6333 10.1 14L12.45 11.65C12.6 11.5 12.7958 11.3875 13.0375 11.3125C13.2792 11.2375 13.5167 11.2167 13.75 11.25L17.2 11.95C17.4333 12.0167 17.625 12.1375 17.775 12.3125C17.925 12.4875 18 12.6833 18 12.9V16.95C18 17.25 17.9 17.5 17.7 17.7C17.5 17.9 17.25 18 16.95 18ZM3.025 6L4.675 4.35L4.25 2H2.025C2.10833 2.68333 2.225 3.35833 2.375 4.025C2.525 4.69167 2.74167 5.35 3.025 6ZM11.975 14.95C12.625 15.2333 13.2875 15.4583 13.9625 15.625C14.6375 15.7917 15.3167 15.9 16 15.95V13.75L13.65 13.275L11.975 14.95Z" fill="#464652"/>
                                         </svg>
                                         <input
-                                            type="text"
+                                            type="tel"
+                                            inputMode="tel"
+                                            autoComplete="tel"
                                             name="phone_number"
                                             placeholder="Nomor HP"
                                             value={phone_number}
@@ -580,6 +584,8 @@ class EditProfile extends Component {
                                         </svg>
                                         <input
                                             type="text"
+                                            inputMode="text"
+                                            autoComplete="off"
                                             name="id_line"
                                             placeholder="ID Line"
                                             value={id_line}
@@ -604,6 +610,8 @@ class EditProfile extends Component {
                                         </svg>
                                         <input
                                             type="text"
+                                            inputMode="text"
+                                            autoComplete="off"
                                             name="id_discord"
                                             placeholder="ID Discord"
                                             value={id_discord}
@@ -628,6 +636,8 @@ class EditProfile extends Component {
                                         </svg>
                                         <input
                                             type="text"
+                                            inputMode="text"
+                                            autoComplete="off"
                                             name="id_instagram"
                                             placeholder="ID Instagram"
                                             value={id_instagram}
@@ -699,6 +709,8 @@ class EditProfile extends Component {
                                         </svg>
                                         <input
                                             type="text"
+                                            inputMode="text"
+                                            autoComplete="organization"
                                             name="nama_sekolah"
                                             placeholder="Nama Sekolah/Institusi"
                                             value={nama_sekolah}
