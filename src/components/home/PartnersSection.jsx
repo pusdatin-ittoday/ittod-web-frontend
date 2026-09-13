d614ace2b03d15a0d04e5df952268cee195be417
CahyoAditya <adityacahyo104@gmail.com>
2026-09-13 18:09:45 +0700

    fix: static animation

--- src/components/home/PartnersSection.jsx
+++ src/components/home/PartnersSection.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion as Motion } from "motion/react";
 import {
-  hoverLift,
   popIn,
   revealUp,
   staggerContainer,
@@ -49,27 +48,15 @@ const PartnersSection = () => {
             <Motion.div
               key={sponsor.name}
               variants={popIn}
-              whileHover="hover"
               whileTap={tapPress}
-              className="group h-28 md:h-36"
+              className="group flex h-28 items-center justify-center border-[3px] border-black bg-white p-4 shadow-[5px_5px_0_#111] transition-[background-color,box-shadow] duration-150 hover:bg-[#F5C518] hover:shadow-[7px_7px_0_#111] md:h-36"
             >
-              <Motion.div
-                variants={{
-                  hidden: {},
-                  visible: { x: 0, y: 0, rotate: 0 },
-                  hover: hoverLift,
-                }}
-                className="flex h-full w-full items-center justify-center border-[3px] border-black bg-white p-4 shadow-[5px_5px_0_#111] transition-[background-color,box-shadow] duration-300 ease-out group-hover:bg-[#F5C518] group-hover:shadow-[7px_7px_0_#111]"
-              >
-                <img
-                  src={sponsor.logo}
-                  alt={sponsor.name}
-                  className={`h-full w-full object-contain transition-transform duration-500 ease-out group-hover:rotate-2 ${
-                    sponsor.className || "group-hover:scale-110"
-                  }`}
-                  loading="lazy"
-                />
-              </Motion.div>
+              <img
+                src={sponsor.logo}
+                alt={sponsor.name}
+                className="h-full w-full object-contain transition-transform duration-150 will-change-transform group-hover:-translate-y-1 group-hover:scale-110"
+                loading="lazy"
+              />
             </Motion.div>
           ))}
         </Motion.div>
